e025fba54aa03a3d7f3c180a320db8169ce4e783
farah-naz123 <fnaz29103@gmail.com>
2024-10-14 22:00:49 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,20 @@
+
+
+const HomePage=() => {
+  return(
+    <div>
+
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/service">Service</a></li>
+
+
+
+
+      </ul>
+    </div>
+  )
+};
+export default HomePage;
